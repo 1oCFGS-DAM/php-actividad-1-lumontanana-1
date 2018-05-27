89dd3c0812d0d98caf925893281af1ed1a884422
Lucía Montañana Fuentes <39645648+lumontanana@users.noreply.github.com>
2018-05-27 13:53:28 +0200

    Crea solucion.php
    Solución propuesta

---
+++ solucion.php
@@ -0,0 +1,28 @@
+<?php
+function cabeceraPagina()
+{
+    return '<!DOCTYPE html><html lang="es">';
+}
+
+function cuerpoPagina($contenido)
+{
+  return '<body>$contenido</body>';
+}
+
+function piePagina()
+{
+    return '</html>';
+}
+
+function multiplos($numero, $tope)
+{
+    $salida = '<ul>';
+    for ($i = $numero; $i < $tope; $i++) {
+        $salida .= $i % $numero === 0 ? "<li>$i</li>" : '';
+    }
+    return $salida . '</ul>';
+}
+
+echo cabeceraPagina();
+echo cuerpoPagina(multiplos(17, 100));
+echo piePagina();
